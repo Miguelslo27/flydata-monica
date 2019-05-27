5c7557b9e819021729fd79aadc66462d9dfa3459
Miguel <miguelmail2006@gmail.com>
2019-05-27 19:51:43 -0300

    new-project/flight-data-md - Cambie por promesa el add de la base

--- flight-data-md-project/index.js
+++ flight-data-md-project/index.js
@@ -28,10 +28,16 @@ server.post('/service-interface-registry', async function (req, res) {
   const data = new iServiceInterface(req.body);
   const client = await mongoDb();
   const collection = client.db("FlightDataMW").collection('service-interface');
-
-  collection.insertOne(data)
   
-  return res.status(200).send('ok');
+  collection.insertOne(null)
+    .then(function (success) {
+      client.close();
+      return res.status(200).send('ok');
+    })
+    .catch(function (error) {
+      client.close();
+      return res.status(500).send('error');
+    });
 });
 
 server.listen(5001, function () {
