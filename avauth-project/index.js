65e78a9bb6f3405e8af52e00584e2f452e5472ed
Miguel <miguelmail2006@gmail.com>
2019-06-17 08:56:56 -0300

    new-project/flight-data-md - Sending data from database by settings

--- avauth-project/index.js
+++ avauth-project/index.js
@@ -7,6 +7,7 @@ const MongoClient = require('mongodb').MongoClient;
 const airlines = require('./mocks/airlines_mock.json');
 const airports = require('./mocks/airports_mock.json');
 const clients = require('./mocks/clients_mock.json');
+const config = require('./config.json');
 
 server.use(express.urlencoded());
 server.use(express.json());
@@ -21,7 +22,7 @@ async function mongoDb() {
   var options = {
     useNewUrlParser: true
   };
-  
+
   const client = await MongoClient.connect(url, options);
   return client;
 }
@@ -38,36 +39,99 @@ async function mongoDb() {
 
 */
 
-// const checkTimer = setInterval(function () {
-  const client = mongoDb();
+let page = config.OFFSET / config.LOTS_SIZE;
+let offset = config.OFFSET;
 
+function processLot() {
+  console.log("Page", page);
+  console.log("config.MAX_LOTS", config.MAX_LOTS);
+
+  if (page == config.MAX_LOTS) {
+    return;
+  }
+
+  const client = mongoDb();
+  const emptyFilter = [
+    { $ne: "" },
+    { $ne: undefined },
+    { $ne: null }
+  ];
+  const norEmptyFilter = {
+    $nor: emptyFilter
+  };
+  const orEmptyFilter = {
+    $or: emptyFilter
+  };
+  
   client
     .then(async function (mongoclient) {
       const flightsCollection = mongoclient.db(dbName).collection('flights');
-      const flightsCount = await flightsCollection.countDocuments();
-      // Filtrar en el find por registros que no tengan timestamp
-      const flights = await flightsCollection.find().toArray();
+      // const flightsCount = await flightsCollection.countDocuments();
 
-      console.log(flights);
+      // Filtrar en el find por registros que no tengan timestamp
+      const flights = await flightsCollection.find({
+        YEAR: norEmptyFilter,
+        MONTH: norEmptyFilter,
+        DAY: norEmptyFilter,
+        DAY_OF_WEEK: norEmptyFilter,
+        AIRLINE: norEmptyFilter,
+        FLIGHT_NUMBER: norEmptyFilter,
+        TAIL_NUMBER: norEmptyFilter,
+        ORIGIN_AIRPORT: norEmptyFilter,
+        DESTINATION_AIRPORT: norEmptyFilter,
+        SCHEDULED_DEPARTURE: norEmptyFilter,
+        DEPARTURE_TIME: norEmptyFilter,
+        DEPARTURE_DELAY: norEmptyFilter,
+        TAXI_OUT: norEmptyFilter,
+        WHEELS_OFF: norEmptyFilter,
+        SCHEDULED_TIME: norEmptyFilter,
+        ELAPSED_TIME: norEmptyFilter,
+        AIR_TIME: norEmptyFilter,
+        DISTANCE: norEmptyFilter,
+        WHEELS_ON: norEmptyFilter,
+        TAXI_IN: norEmptyFilter,
+        SCHEDULED_ARRIVAL: norEmptyFilter,
+        ARRIVAL_TIME: norEmptyFilter,
+        ARRIVAL_DELAY: norEmptyFilter,
+        DIVERTED: norEmptyFilter,
+        CANCELLED: norEmptyFilter,
+        // CANCELLATION_REASON: norEmptyFilter,
+        AIR_SYSTEM_DELAY: norEmptyFilter,
+        SECURITY_DELAY: norEmptyFilter,
+        AIRLINE_DELAY: norEmptyFilter,
+        LATE_AIRCRAFT_DELAY: norEmptyFilter,
+        WEATHER_DELAY: norEmptyFilter,
+        UPDATED: orEmptyFilter
+      }).skip(offset).limit(config.LOTS_SIZE).toArray();
+
+      // console.log(flights);
+      // console.log("Offset", offset);
+      console.log("Flights", flights.length);
+
       // Valido los datos y filtro antes de enviarlos
-      
-      // Les agrego el timestam a los registros y los actualizo
-      
+
+      // Les agrego el timestamp a los registros y los actualizo
+
       // Luego se los envio a todos los clientes
-      clients.forEach(function (client) {
-        axios
-          .post(client['service-url'], flights)
-          .then(function (response) {
-            console.log(response);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-      });
-      
+      // clients.forEach(function (client) {
+      //   axios
+      //     .post(client['service-url'], flights)
+      //     .then(function (response) {
+      //       console.log(response);
+      //     })
+      //     .catch(function (error) {
+      //       console.log(error);
+      //     });
+      // });
+
       if (client.close) {
         client.close();
       }
+
+      page++;
+      offset = page * config.LOTS_SIZE;
+
+      setTimeout(processLot, config.FREQUENCY);
     })
     .catch(function (error) {
       if (client.close) {
@@ -75,7 +139,9 @@ async function mongoDb() {
       }
       console.log(error);
     });
-// }, 3000);
+}
+
+processLot();
 
 server.get('/', function (req, res) {
   return res.status(200).send(`
@@ -116,7 +182,7 @@ server.get('/airlines/:id', function (req, res) {
   if (!airline) {
     return res.status(404).send({ status: 'error', message: 'Airline not found' });
   }
-  
+
   return res.status(200).send(airline);
 });
 
